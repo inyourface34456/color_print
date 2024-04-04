0124ae3139b42d34dce2ea4e6d42a551a3a5fbb5
inyourface34456 m <trenten_miller@etownstudents.org>
2024-04-04 18:16:59 +0000

    first test run

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 wai_bindgen_rust::export!("color_print.wai");
+
+struct ColorPrint;
+
+impl crate::color_print::ColorPrint for ColorPrint {
+    fn test() -> () {
+        println!("this is cool")
+    }
+}
